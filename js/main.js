8f5c1943b022bc039e8983f6bc4d3526b38b7b42
Dustin <dmkelly100@gmail.com>
2019-12-14 11:00:14 -0500

    uploading all files

---
+++ js/main.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+    function removeTransition(e) {
+        if (e.propertyName !== 'transform') return;
+        e.target.classList.remove('playing');
+      }
+      
+      function playSound(e) {
+        let keyCode;
+        if (e.type === 'click') {
+          keyCode = e.currentTarget.dataset.key;
+        } else {
+          keyCode = e.keyCode;
+        }
+      
+        const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+        const key = document.querySelector(`div[data-key="${keyCode}"]`);
+        if (!audio) return;
+      
+        key.classList.add('playing');
+        audio.currentTime = 0;
+        audio.play();
+      }
+      
+      const keys = Array.from(document.querySelectorAll('.key'));
+      keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+      window.addEventListener('keydown', playSound);
+      keys.forEach(key => key.addEventListener('click', playSound))
+        
+      
+};
